fa49023090ac5f008869cc7833f3266ee7228b6f
Jean Caiza <jeanmcm@hotmail.com>
2025-08-16 16:38:53 -0500

    fix: add default components

--- packages/form-kit/src/form-kit.tsx
+++ packages/form-kit/src/form-kit.tsx
@@ -35,7 +35,7 @@ export const Form = <
     onSubmit,
     onCancel,
     fieldTransformer,
-    components,
+    components = {},
   } = props;
 
   const initialFormValues = useMemo<z.input<Z>>(() => {
